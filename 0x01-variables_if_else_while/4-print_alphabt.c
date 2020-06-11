51d061976c6d8a19526d52ce913a476d6d112339
MarcosPimienta <1676@holbertonschool.com>
2020-06-11 22:19:57 +0000

    exclude e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char alp = 'a';
+
+	while (alp <= 'z')
+	{
+		if (alp != 'e' && alp != 'q')
+		{
+			putchar(alp);
+		}
+		alp++;
+	}
+		putchar('\n');
+	return (0);
+}
