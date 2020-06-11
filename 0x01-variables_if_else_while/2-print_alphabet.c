e64464280d9dbf0e47a3f044faeebd8b642d78cd
MarcosPimienta <1676@holbertonschool.com>
2020-06-11 19:43:41 +0000

    betty fixed

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char alp = 'a';
+
+	while (alp <= 'z')
+	{
+		putchar(alp);
+		alp++;
+	}
+		putchar('\n');
+	return (0);
+}
