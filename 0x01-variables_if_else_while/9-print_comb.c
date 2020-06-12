d02e9bb2a27699b4853daa99a4937f55f49c2726
MarcosPimienta <1676@holbertonschool.com>
2020-06-12 02:02:35 +0000

    betty fixed

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,18 +8,18 @@
 
 int main(void)
 {
-	int n = 0 + '0';
+int n = 0 + '0';
 
-	while (n <= '9')
-	{
-		putchar(n);
-        if(n <= '8')
-        {
-        putchar(',');
-        }
-        putchar(' ');
-		n++;
-	}
-		putchar('\n');
-	return (0);
+while (n <= '9')
+{
+putchar(n);
+if (n <= '8')
+{
+putchar(',');
+putchar(' ');
+}
+n++;
+}
+putchar('\n');
+return (0);
 }
