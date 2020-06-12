67cf70dbe2272f2217511a8142c7a854bbb38c8f
MarcosPimienta <1676@holbertonschool.com>
2020-06-12 01:01:33 +0000

    reverse alpha

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char alp = 'z';
+
+	while (alp >= 'a')
+	{
+		putchar(alp);
+		alp--;
+	}
+		putchar('\n');
+	return (0);
+}
