fac597d4012cb8681c9185e62b51404d3039d0f1
MarcosPimienta <1676@holbertonschool.com>
2020-06-13 02:00:05 +0000

    scalator

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int n = 0;
+int x = 2;
+
+while (n <= 89)
+{
+	if (n % 10 == 0 && n / 10 != 0)
+	{
+		n = n + x ;
+		x++;
+	}
+	putchar((n / 10) + '0');
+	putchar((n % 10) + '0');
+		if (n < 89)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	n++;
+}
+putchar('\n');
+return (0);
+}
