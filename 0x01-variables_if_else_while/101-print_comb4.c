3c0cb68a20c6d769e7382978ebc4f873ad190f87
MarcosPimienta <1676@holbertonschool.com>
2020-06-15 02:06:51 +0000

    fixed Betty

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,9 +1,9 @@
-#include <stdio.h>     
+#include <stdio.h>
 #include <stdlib.h>
 /**
- *main-prints number 01 to 89
- *Return: 0
- */
+*main-prints number 01 to 89
+*Return: 0
+*/
 int main(void)
 {
 	int x = 0;
@@ -12,17 +12,17 @@ int main(void)
 
 	while (x <= 7)
 	{
-		while(y <= 8) 
+		while (y <= 8)
 		{
-			while(z <= 9)
+			while (z <= 9)
 			{
-				putchar(x + '0');
-				putchar(y + '0');
-				putchar(z + '0');
-				if((x != 7) || (y != 8) || (z != 9))
+				putchar (x + '0');
+				putchar (y + '0');
+				putchar (z + '0');
+				if ((x != 7) || (y != 8) || (z != 9))
 				{
-				putchar(',');
-				putchar(' ');
+				putchar (',');
+				putchar (' ');
 				}
 				z++;
 			}
@@ -30,8 +30,8 @@ int main(void)
 			z = y + 1;
 		}
 		x++;
-		y = x +1;
-		z = y +1;
+		y = x + 1;
+		z = y + 1;
 	}
 	putchar('\n');
 	return (0);
