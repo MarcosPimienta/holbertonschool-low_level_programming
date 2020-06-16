5a5501ae2264c91e2906af8c80d2069e27bfe61c
MarcosPimienta <1676@holbertonschool.com>
2020-06-16 15:58:55 +0000

    fixed the repo

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet - prints alphabet with putchar
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+    char alp = 'a';
+
+    while (alp <= 'z')
+    {
+        _putchar(alp);
+        alp++;
+    }
+    _putchar('\n');
+}
