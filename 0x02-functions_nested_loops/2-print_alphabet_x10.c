c958a1316ba9ddf5109fc52786517eb1e690daf0
MarcosPimienta <1676@holbertonschool.com>
2020-06-16 17:05:55 +0000

    fixed betty

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,15 +10,15 @@ void print_alphabet_x10(void)
 char alp = 'a';
 int i = 0;
 
-while ( i <= 9)
+while (i <= 9)
 {
-    while (alp <= 'z')
-    {
-    _putchar(alp);
-    alp++;
-    }
-    alp = 'a';
-    i++;
-    _putchar('\n');
+while (alp <= 'z')
+{
+_putchar(alp);
+alp++;
+}
+alp = 'a';
+i++;
+_putchar('\n');
 }
 }
