e652b26863fa302cd70b4824769ead65423d92cc
MarcosPimienta <1676@holbertonschool.com>
2020-06-16 17:53:57 +0000

    islowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,21 +1,21 @@
 #include "holberton.h"
 
 /**
-* int _islower - prints awhich is lower
+* _islower - prints if c is lowercase
+* @c: variable
 * Return: Always 0.
 */
 
 int _islower(int c)
 {
 
-    if (c > 96 && c < 123)
-    {
-        return (1);
-    }
-    else
-    {
-        return (0);
-    }
-    return (0);
-    _putchar('\n');
+if (c > 96 && c < 123)
+{
+return (1);
+}
+else
+{
+return (0);
+_putchar('\n');
+}
 }
