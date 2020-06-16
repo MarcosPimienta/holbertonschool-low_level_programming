8e70b18f037bbf5af1ffb4d07e6aa46bfb73633f
MarcosPimienta <1676@holbertonschool.com>
2020-06-16 15:28:04 +0000

    fixed name

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+* main - entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int i;
+char c[] = "Holberton";
+for (i = 0; i <= 8; i++)
+{
+_putchar(c[i]);
+}
+_putchar('\n');
+return (0);
+}
