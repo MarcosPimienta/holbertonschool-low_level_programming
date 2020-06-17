00e8b4f0de7d080ae94b0ad41fc3ff357d8190e4
MarcosPimienta <1676@holbertonschool.com>
2020-06-17 01:05:11 +0000

    times_table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+
+/**
+* times_table - prints table
+*
+* Return: Always 0.
+*/
+
+void times_table(void)
+{
+int h, v;
+int n = 0;
+
+	for (h = 0; h <= 9; h++)
+	{
+		for (v = 0; v <= 9; v++)
+		{
+			n = h * v;
+			if (n > 9)
+			{
+			_putchar ((n / 10) + '0');
+			_putchar ((n % 10) + '0');
+				if (v <= 8)
+				{
+				_putchar (',');
+				_putchar (' ');
+				}
+			}
+			if (n <= 9)
+			{
+			_putchar (n + '0');
+				if (v <= 8)
+				{
+					_putchar (',');
+					_putchar (' ');
+					_putchar (' ');
+				}
+			}
+		}
+		_putchar ('\n');
+	}
+}
