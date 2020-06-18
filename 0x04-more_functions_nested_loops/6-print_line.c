32941e574e73994e1892af64122a6c8bec92a3b5
MarcosPimienta <1676@holbertonschool.com>
2020-06-18 20:34:51 +0000

    fixed betty

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -13,14 +13,14 @@ int i;
 
 if (n <= 0)
 {
-    _putchar('\n');
+_putchar('\n');
 }
 else
 {
-    for (i = 0; i <= n; i++)
-    {
-        _putchar('_');
-    }
-    _putchar('\n');
+for (i = 0; i <= n; i++)
+{
+_putchar('_');
+}
+_putchar('\n');
 }
 }
