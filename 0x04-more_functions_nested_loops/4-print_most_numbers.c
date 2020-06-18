b9a61b45043cea7f9ce1b644741af91c4ff1eae9
MarcosPimienta <1676@holbertonschool.com>
2020-06-18 17:59:43 +0000

    added to print most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+*print_most_numbers - prints numbers except 2 & 4
+*
+*Return: Always 0 (Success)
+*/
+void print_most_numbers(void)
+{
+int n;
+
+for (n = 0; n <= 9; n++)
+{
+	if(n != 2 && n != 4)
+	_putchar(n + '0');
+}
+_putchar('\n');
+}
