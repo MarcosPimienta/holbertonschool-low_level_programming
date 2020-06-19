17591ba8acb69d0da4a55d7ea6c6d1424cb650ea
MarcosPimienta <1676@holbertonschool.com>
2020-06-19 04:49:51 +0000

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+*main - main function
+*
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int i;
+int n = 0;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("FizzBuzz ");
+			n = 1;
+		}
+		if ((i % 3 != 0) && (i % 5 != 0) && (n == 0))
+		{
+			printf("%d ", i);
+			n = 0;
+		}
+		if ((i % 3 == 0) && (n == 0))
+		{
+			printf("Fizz ");
+		}
+		if ((i % 5 == 0) && (n == 0))
+		{
+			printf("Buzz ");
+		}
+		n = 0;
+	}
+return (0);
+}
