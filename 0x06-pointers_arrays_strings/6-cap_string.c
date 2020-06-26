b8ca35e103728b0cab55ac7fd22178e7dff861d0
MarcosPimienta <1676@holbertonschool.com>
2020-06-26 02:31:58 +0000

    fixed

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+/**
+ * cap_string - check the code for Holberton School students.
+ * @s: Argument pointer to char
+ *
+ * Return: Always 0.
+ */
+char *cap_string(char *s)
+{
+	int i;
+	int p = 32;
+	for (i = 0; s[i]; i++)
+	{
+		if (((s[i] >= 'a') && (s[i] <= 'z'))
+			&& ((s[i - 1] == ' ') || (s[i - 1] == '\n')
+			|| (s[i - 1] == ',') || (s[i - 1] == ';')
+			|| (s[i - 1] == '.') || (s[i - 1] == '!')
+			|| (s[i - 1] == '?') || (s[i - 1] == '"')
+			|| (s[i - 1] == '(') || (s[i - 1] == ')')
+			|| (s[i - 1] == '{') || (s[i - 1] == '}')
+			|| (s[i - 1] == '\t')))
+		{
+			s[i] = s[i] - p;
+		}
+        else if ((s[i] >= 97 && s[i] <= 122) && i == 0)
+        {
+            s[i] = s[i] - p
+        }
+		else
+		{
+			s[i] = s[i];
+		}
+	}
+return (s);
+}
