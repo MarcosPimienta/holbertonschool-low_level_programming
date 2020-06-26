4eb4da6f2784f27b130a4e47f5f51294cbce6467
MarcosPimienta <1676@holbertonschool.com>
2020-06-26 04:30:03 +0000

    fixed betty

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -1,29 +1,29 @@
 #include "holberton.h"
 /**
- * leet - encrypts.
+ * rot13 - encrypts caesar code.
  * @s: Argument pointer to char
  *
  * Return: Always 0.
  */
 char *rot13(char *s)
 {
-	int i;
-	int j;
-	char ch1[] = "ABCDEFGHIJKLMNOPQRSTUWXYZabcdefghijklmnopqrstuwxyz";
-	char ch2[] = "NOPQRSTUVWXYZABCDEFGHJKLMnopqrstuvwxyzabcdefghjklm";
+int i;
+int j;
+char ch1[] = "ABCDEFGHIJKLMNOPQRSTUWXYZabcdefghijklmnopqrstuwxyz";
+char ch2[] = "NOPQRSTUVWXYZABCDEFGHJKLMnopqrstuvwxyzabcdefghjklm";
 
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		j = 0;
-		while (ch1[j] != '\0')
-		{
-			if (s[i] == ch1[j])
-			{
-				s[i] = ch2[j];
-				break;
-			}
-			j++;
-		}
-	}
+for (i = 0; s[i] != '\0'; i++)
+{
+j = 0;
+while (ch1[j] != '\0')
+{
+if (s[i] == ch1[j])
+{
+s[i] = ch2[j];
+break;
+}
+j++;
+}
+}
 return (s);
 }
