8c1c9c5a03a5ac122d4b5f54de3161849d16aa7e
MarcosPimienta <1676@holbertonschool.com>
2020-06-28 21:14:21 +0000

    fixed betty

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 
 /**
-* *_memset - fill a block of memory with a particular value.
-*@s: - Declaration of variable
-*@b: - Declaration of variable type
+* *_memcpy - copy a block of memory with a particular value.
+*@dest: - Declaration of variable
+*@src: - Declaration of variable type
 *@n: - Size of bytes
 *Return: Always s for string
 */
@@ -14,7 +14,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	char *tmp1 = dest;
 	char *tmp2 = src;
 
-	for ( i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		*tmp1 = *tmp2;
 		tmp1++;
