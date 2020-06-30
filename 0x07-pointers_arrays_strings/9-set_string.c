0825a11dad389d36ea25a999678c11e1664b92a9
MarcosPimienta <1676@holbertonschool.com>
2020-06-30 03:15:30 +0000

    done for today

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * set_string - set string value to another string.
+ * @s: array
+ * @to: size of array
+ */
+void set_string(char **s, char *to)
+{
+
+*s = to;
+
+}
