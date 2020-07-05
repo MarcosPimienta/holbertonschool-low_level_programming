ba6f3ec354869848706fd5ee5cd26b4b5a248473
MarcosPimienta <1676@holbertonschool.com>
2020-07-05 21:18:38 +0000

    mynameis

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+*main - function that takes power elevated.
+*@argc: - Declaration of variable
+*@argv: - Declaration of variable
+*Return: int
+*/
+
+int main(int argc, char **argv)
+{
+if (argc != '\n')
+{
+printf("%s\n", *argv++);
+}
+return (0);
+}
