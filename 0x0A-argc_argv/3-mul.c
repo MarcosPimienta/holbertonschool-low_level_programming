5364616b9a226b4e6e1bc1a1850a482a62c1578a
MarcosPimienta <1676@holbertonschool.com>
2020-07-06 01:21:41 +0000

    multiply numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
-*main - function that takes power elevated.
+*main - function that prints argc numbers.
 *@argc: - Declaration of variable
 *@argv: - Declaration of variable
 *Return: int
@@ -10,10 +10,18 @@
 
 int main(int argc, char **argv)
 {
-while (argc && *argv)
+	int mult;
+
+if (argc != 3)
 {
-printf("%s\n", *argv);
-argv++;
+	puts("Error");
+	return (1);
+}
+
+else
+{
+	mult = atoi(argv[1]) * atoi(argv[2]);
+	printf("%d\n", mult);
 }
 return (0);
 }
