9cb1125459ed8e5295ab7c0eb1335340ea725a54
MarcosPimienta <1676@holbertonschool.com>
2020-07-08 23:27:19 +0000

    fixed betty

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* alloc_grid - returns a pointer to a 2 dimensional array of integers.
+*Return: always newstr.
+*@width : char pointer variable to  new array.
+*@height : char pointer variable to  new array.
+*/
+
+void free_grid(int **grid, int height)
+{
+	for (j = 0; j < i; j++)
+	{
+	free(p[j]);
+	}
+	free(p);
+	return (NULL);
+}
