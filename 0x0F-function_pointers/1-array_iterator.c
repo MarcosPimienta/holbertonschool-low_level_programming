1c05068699bbc23b550c0910a0aacfc4e8106504
MarcosPimienta <1676@holbertonschool.com>
2020-07-15 23:22:44 +0000

    learning

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -6,14 +6,17 @@
 * @array: elements to be printed.
 * @size: size of.
 * @action: function for each element of the array.
+*
 * Return: Nothing.
 */
+
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	if (array && action && size != 0)
 	{
 		size_t i = 0;
-		while(i < size)
+
+		while (i < size)
 		{
 			action(array[i]);
 			i++;
