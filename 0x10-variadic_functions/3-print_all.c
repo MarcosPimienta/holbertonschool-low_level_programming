d2ca0e3d71823deaee8defecd9a23dd7cc0c557b
MarcosPimienta <1676@holbertonschool.com>
2020-07-18 21:45:09 +0000

    fixing stuff

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -7,43 +7,39 @@
 */
 void print_all(const char * const format, ...)
 {
-	int i = 0, j = 0;
+	unsigned int i = 0;
 	va_list arguments;
-	char *str = NULL;
+	char *str;
 
 	va_start(arguments, format);
-	while (format[i])
-		i++;
-	while (format[j])
+
+	while (format && format[i])
 	{
-		switch (format[j])
+		switch (format[i++])
 		{
 			case 'c':
-			printf("%c", (char)va_arg(arguments, int));
+			printf("%c", va_arg(arguments, int));
 			break;
 			case 'i':
-			printf("%i", va_arg(arguments, int));
+			printf("%d", va_arg(arguments, int));
 			break;
 			case 'f':
-			printf("%f", (float)va_arg(arguments, int));
+			printf("%f", (float)va_arg(arguments, double));
 			break;
 			case 's':
-			str = va_arg(arguments, char*);
-			if (str == NULL)
+			str = va_arg(arguments, char *);
+			if (!str)
 			{
-				printf("(nil)");
-				break;
+				str = "(nil)";
 			}
 			printf("%s", str);
 			break;
-			}
-			if ((format[j] == 'c' || format[j] == 'i' ||
-				format[j] == 'f' || format[j] == 's') && (j < (i - 1)))
+		}
+			if (format[i])
 			{
 				printf(", ");
 			}
-		j++;
 	}
-	va_end(arguments);
 	printf("\n");
+	va_end(arguments);
 }
