254c71b8f54702783e33ca85d5da15883426405a
MarcosPimienta <1676@holbertonschool.com>
2020-07-31 03:12:49 +0000

    flips

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ *clear_bit -clears value of a bit to 0 at a given index.
+ *@index: index of bit find
+ *@n: unsigned long integer
+ *
+ *Return: 1 if it worked, or -1 if an error occurred
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+
+if (index > sizeof(unsigned long int) * 8 || n == NULL)
+return (-1);
+
+*n = *n & ~(1 << index);
+	return (1);
+
+}
---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ *flip_bits - the value of a bit to 0 at a given index.
+ *@n: pointer to unsigned long integer
+ *@m: index of intger to sets
+ *
+ *Return: num which is number of bits that where different.
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int i = 0;
+	int num = 0;
+
+	while (i < (sizeof(n) * 8))
+	{
+		if ((((n ^ m) >> i) & 1) == 1)
+		{
+			num++;
+		}
+        i++;
+	}
+	return (num);
+}
