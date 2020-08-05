e35e847c570bea02c10f9d395467a1ba259366b0
MarcosPimienta <1676@holbertonschool.com>
2020-08-05 00:39:04 +0000

    fixed code

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -9,11 +9,4 @@
 
 /* reads and prints a text file*/
 ssize_t read_textfile(const char *filename, size_t letters);
-
-/* creates a file with read and write permissions*/
-int create_file(const char *filename, char *text_content);
-
-/* append text at thge end of file content*/
-int append_text_to_file(const char *filename, char *text_content);
-
 #endif
