d4a13eba7b23fc016bfccb0f82863a6df379230c
MarcosPimienta <1676@holbertonschool.com>
2020-09-06 18:14:46 +0000

    connected prev to NULL

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -20,7 +20,9 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	return (NULL);
 
 	newNode->n = n;
+	newNode->prev = NULL;
 	newNode->next = *head;
+
 	*head = newNode;
 
 	return (newNode);
