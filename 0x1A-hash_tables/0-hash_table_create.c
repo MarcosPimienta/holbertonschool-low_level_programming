aae3b41251412d23b475a5a8388649238365b73f
MarcosPimienta <1676@holbertonschool.com>
2020-10-02 17:03:55 +0000

    fixed edge cases

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -14,7 +14,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	ht->array = malloc(sizeof(hash_node_t *) * size);
 	if (!ht->array)
+	{
+		free(ht);
 		return (NULL);
+	}
+	ht->size = size;
 
 	return (ht);
 }
