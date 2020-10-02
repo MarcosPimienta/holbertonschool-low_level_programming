e31d48de5bc64a98998ff609aab8026278c184fb
MarcosPimienta <1676@holbertonschool.com>
2020-10-02 15:57:38 +0000

    fixed malloc Null cases

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,7 +10,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *ht = NULL;
 
 	ht = malloc(sizeof(hash_table_t));
+	if (!ht)
+		return NULL;
 	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (!ht->array)
+		return NULL;
 
 	return (ht);
 }
