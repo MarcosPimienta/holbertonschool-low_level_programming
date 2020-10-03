cf70889823772ba8afb8969755fe3a82d6470525
MarcosPimienta <1676@holbertonschool.com>
2020-10-03 04:34:41 +0000

    delete hash table

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - prints all elements of array
+ * @ht: hash node
+ * Return: 0 if fails, 1 if not
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *aux =  NULL, *tmp = NULL;
+
+	if (!ht)
+		return;
+	if (ht->size == 0 || !ht->array)
+	{
+		free(ht);
+		return;
+	}
+	for (i = 0; i < ht->size; i++)
+	{
+		aux = ht->array[i];
+		while (aux)
+		{
+			tmp = aux;
+			aux = aux->next;
+			free(tmp->key);
+			free(tmp->value);
+			free(tmp);
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
