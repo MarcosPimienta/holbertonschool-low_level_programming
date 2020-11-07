ef1e2b3cebf587764d5460440635b854f7deef40
MarcosPimienta <1676@holbertonschool.com>
2020-11-07 22:20:54 +0000

    island perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -5,25 +5,20 @@ Island Permieter
 
 
 def island_perimeter(grid):
-        """Island Permieter"""
+    """Island Permieter"""
 
-        n = 0
-        for i in range(len(grid)):
-                for j in range(len(grid[i])):
-                        if grid[i][j] is 1:
-                                if i > 0 and i < len(grid) - 1:
-                                        if grid[i - 1][j] is 0:
-                                                n += 1
-                                        if grid[i + 1][j] is 0:
-                                                n += 1
-                                else:
-                                        n += 2
-                                if j > 0 and j < len(grid[i]) - 1:
-                                        if grid[i][j - 1] is 0:
-                                                n += 1
-                                        if grid[i][j + 1] is 0:
-                                                n += 1
-                                else:
-                                        n += 2
+    n = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 0:
+                continue
+            if j == 0 or grid[i][j - 1] == 0:
+                n += 1
+            if j == len(grid[i]) - 1 or grid[i][j + 1] == 0:
+                n += 1
+            if i == 0 or grid[i - 1][j] == 0:
+                n += 1
+            if i == len(grid) - 1 or grid[i + 1][j] == 0:
+                n += 1
 
-        return n
+    return n
