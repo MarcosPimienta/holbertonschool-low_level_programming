a2d98c3e14f421f07661f6b2e738d473364fc180
MarcosPimienta <1676@holbertonschool.com>
2021-01-13 20:20:17 +0000

    fixed NULL case

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -14,12 +14,15 @@ int linear_search(int *array, size_t size, int value)
 {
 	unsigned int i;
 
-	for (i = 0; i <= size; i++)
+	if (array != NULL)
 	{
-		printf("Value checked array[%d] = [%d]\n", i, array[i]);
-		if (array[i] == value)
+		for (i = 0; i <= size; i++)
 		{
-			return (i);
+			printf("Value checked array[%d] = [%d]\n", i, array[i]);
+			if (array[i] == value)
+			{
+				return (i);
+			}
 		}
 	}
 	return (-1);
