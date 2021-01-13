ff087a6b458966b046ac8553c73aebc037a69c85
MarcosPimienta <1676@holbertonschool.com>
2021-01-13 19:58:09 +0000

    new proj

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "search_algos.h"
+
+/**
+ * linear_search - search in an array
+ * @array: data set
+ * @size: size of array
+ * @value: value to be searched
+ * *Return: Always s for string
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	unsigned int i;
+
+	for (i = 0; i <= size; i++)
+	{
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}
